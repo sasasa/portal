21de5e2f5fa824d091c672b622536b5540de5433
masasaeki <masaakisaeki@gmail.com>
2020-07-27 11:36:26 +0900

    店舗のブログ記事入力機能 #4
    ブログから店舗に行けないと不便なのでリンクを追加する
    改行が反映されないのでnl2brする

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -6,7 +6,7 @@
 <img src="/storage/{{$blog->blog_path}}">
 
 <p>
-  {{ $blog->blog_content }}
+  {!! nl2br(e($blog->blog_content)) !!}
 </p>
 
 @if (Auth::id() == $blog->user_id)
@@ -16,6 +16,8 @@
   <input type="submit" value="削除する" class="btn btn-sm btn-danger">
 </form>
 @endif
+
+<a href="/shops/{{$shop->id}}">店舗を見る</a>
 
 @endsection
 
